fd83d69c47961da81c4b1262b18ebcfa343e4e6e
Mochammad Alif Kurniawan <mochammadalifkurniawan@gmail.com>
2021-10-27 00:41:34 +0700

    up

--- 02-variables-constants/constants.c
+++ 02-variables-constants/constants.c
@@ -2,23 +2,22 @@
 // gcc constants.c -o constants; ./constants
 
 // #define
-#define NAME 'LIFZ' 
+#define NAME "LIFZ" 
 #define GENDER 'M' 
 #define GPA 3.65
 
 int main(){
 	// const, types required
 	const double KERNEL = 5.140;
-	const char OS = 'KALI';
+	const char OS = 'L';
 	const int RESOLUTION = 1366;
 	// print #define
 	printf("NAME: %s\n", NAME);
 	printf("GENDER: %c\n", GENDER);
-	printf("GPA %.2f\n", GPA);
+	printf("GPA %.2f", GPA);
+	printf("\n---\n");
 	// print const
-	printf("KERNEL: %f\n", KERNEL);
+	printf("KERNEL: %.3f\n", KERNEL);
 	printf("OS: %c\n", OS);
 	printf("RESOLUTION: %d\n", RESOLUTION);
 }
-
-
